8a908b4ab1283d0337aac227208459ca91759a01
Phamlong9102 <namvu9669@gmail.com>
2022-09-25 20:40:41 +0700

    delete some file dont use

--- src/App.js
+++ src/App.js
@@ -5,55 +5,49 @@ import { DefaultLayout } from '~/components/Layout';
 import { AppContextProvider } from './contexts/AppContext';
 import { ClickGetDataContextProvider } from './contexts/ClickGetDataContext';
 import { AdminContextProvider } from './contexts/AdminContext';
-import { AdminAddProductProvider } from './contexts/AdminAddProductContext';
-import { AdminEditProductProvider } from './contexts/AdminEditProductContext';
 
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
    return (
-      <AdminEditProductProvider>
-         <AdminAddProductProvider>
-            <AdminContextProvider>
-               <ClickGetDataContextProvider>
-                  <AppContextProvider>
-                     <Router>
-                        <div className="App">
-                           <Routes>
-                              {publicRoutes.map((route, index) => {
-                                 const Page = route.component;
-                                 let Layout = DefaultLayout;
+      <AdminContextProvider>
+         <ClickGetDataContextProvider>
+            <AppContextProvider>
+               <Router>
+                  <div className="App">
+                     <Routes>
+                        {publicRoutes.map((route, index) => {
+                           const Page = route.component;
+                           let Layout = DefaultLayout;
 
-                                 if (route.layout) {
-                                    Layout = route.layout;
-                                 } else if (route.layout === null) {
-                                    Layout = Fragment;
+                           if (route.layout) {
+                              Layout = route.layout;
+                           } else if (route.layout === null) {
+                              Layout = Fragment;
+                           }
+
+                           return (
+                              <Route
+                                 key={index}
+                                 path={route.path}
+                                 element={
+                                    // Layout ôm Page lại thì Page trở thành children của
+                                    // Layout. Vì nó trở thành children nên nó sẽ được đưa vào
+                                    // {children} của DefaultLayout và được đưa vào phần content
+                                    <Layout>
+                                       <Page />
+                                    </Layout>
                                  }
-
-                                 return (
-                                    <Route
-                                       key={index}
-                                       path={route.path}
-                                       element={
-                                          // Layout ôm Page lại thì Page trở thành children của
-                                          // Layout. Vì nó trở thành children nên nó sẽ được đưa vào
-                                          // {children} của DefaultLayout và được đưa vào phần content
-                                          <Layout>
-                                             <Page />
-                                          </Layout>
-                                       }
-                                    />
-                                 );
-                              })}
-                           </Routes>
-                        </div>
-                     </Router>
-                  </AppContextProvider>
-               </ClickGetDataContextProvider>
-            </AdminContextProvider>
-         </AdminAddProductProvider>
-      </AdminEditProductProvider>
+                              />
+                           );
+                        })}
+                     </Routes>
+                  </div>
+               </Router>
+            </AppContextProvider>
+         </ClickGetDataContextProvider>
+      </AdminContextProvider>
    );
 }
 
